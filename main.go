62a2ca4cd39f326760bf8fead77b32eba76686c5
Chamod Jayampathi <76867432+jayampathi-bac@users.noreply.github.com>
2021-01-29 10:55:15 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import(
+import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gocolly/colly"
+	"log"
 )
 func main() {
 	var location string;
@@ -13,8 +16,31 @@ func main() {
 	fmt.Print("Enter Category : ")
 	fmt.Scan(&category)
 
+	//c.OnHTML(".gtm-normal-ad", func(element *colly.HTMLElement) {
+	//	fmt.Println(element.Text,"\n")
+	//})
+	////////////////////////////////////////////////////////////////////////////////////
+	db, err := sql.Open("mysql", "root:ijse@tcp(127.0.0.1:3306)/ikmandb")
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	///////////////////////////////////////////////////////////////////////////////////
 	c.OnHTML(".gtm-normal-ad", func(element *colly.HTMLElement) {
-		fmt.Println(element.Text,"\n")
+		title:= element.ChildText(".heading--2eONR")
+		descr:= element.ChildText(".description--2-ez3")
+		price :=element.ChildText(".price--3SnqI")
+		fmt.Println("Item============================================")
+		fmt.Println("\tTitle: ",title)
+		fmt.Println("\tDescription: ",descr )
+		fmt.Println("\tPrice: ",price )
+		sql,err := db.Query("INSERT INTO item(title, descr, price,location, category) VALUES (?,?,?,?,?)",title, descr, price,location, category)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer sql.Close()
+
 	})
 
 	c.Visit("https://ikman.lk/en/ads/"+ location +"/" +category);
